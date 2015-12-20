7220ca6c734cacd71064ebd74e9d620672a5b34e
Javier Domingo Cansino <javierdo1@gmail.com>
2015-12-20 19:51:36 +0100

    Upgrading android version, using butterknife for view injection

--- app/src/main/java/com/besga/jonander/avisaaane/MainActivity.java
+++ app/src/main/java/com/besga/jonander/avisaaane/MainActivity.java
@@ -1,65 +1,49 @@
 package com.besga.jonander.avisaaane;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.parse.Parse;
-import com.parse.ParseInstallation;
 import com.parse.ParsePush;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnYes;
-    private Button btn_disable;
-    private Button btn_notify;
-    private TextView tv_status;
+    @Bind(R.id.tv_status)
+    TextView tv_status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
 
-        btnYes = (Button) this.findViewById(R.id.btn_yes);
+    @OnClick(R.id.btn_yes)
+    public void subscribe(View view) {
+        ParsePush.subscribeInBackground("Ane");
+        tv_status.setText("Suscrita a las notificaciones");
+    }
 
-        btn_disable = (Button) this.findViewById(R.id.btn_disable);
-        btn_notify = (Button) this.findViewById(R.id.btn_notify);
-        tv_status = (TextView) this.findViewById(R.id.tv_status);
+    @OnClick(R.id.btn_disable)
+    public void notify(View view) {
+        ParsePush.unsubscribeInBackground("Ane");
+        tv_status.setText("Cancelar suscripcion");
 
-        btnYes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ParsePush.subscribeInBackground("Ane");
+    }
 
-                tv_status.setText("Suscrita a las notificaciones");
-            }
-        });
-
-        btn_notify.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ParsePush push = new ParsePush();
-                push.setChannel("Ane");
-                push.setMessage("TE ESTOY LLAMANDO ANE.");
-                push.sendInBackground();
-
-                tv_status.setText("Notificacion enviada");
-            }
-        });
-
-        btn_disable.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ParsePush.unsubscribeInBackground("Ane");
-
-                tv_status.setText("Cancelar suscripcion");
-            }
-        });
-
-
+    @OnClick(R.id.btn_notify)
+    public void unsubscribe(View view) {
+        ParsePush push = new ParsePush();
+        push.setChannel("Ane");
+        push.setMessage("TE ESTOY LLAMANDO ANE.");
+        push.sendInBackground();
+        tv_status.setText("Notificacion enviada");
     }
 }
